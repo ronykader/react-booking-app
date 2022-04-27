b3ce523d716fe5b990a06e9190530dff9e88a5c0
ronykader <rony.ksr06@gmail.com>
2022-04-28 00:58:33 +0600

    added navbar and header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,48 @@
+import {
+  faBed,
+  faCar,
+  faPlane,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport taxi</span>
+          </div>
+        </div>
+        <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
+        <p className="headerDesc">
+          Get rewarded for your travels & unlock instant savings of 10% or with
+          a free w3zonesbooking account
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
